f78cbf04dcba8e1aad922a79b0cecf5664215406
Matthew Burns <mattcburns@gmail.com>
2022-11-26 05:36:12 +0000

    added somel notes

--- main.go
+++ main.go
@@ -5,3 +5,13 @@ import "fmt"
 func main() {
 	fmt.Println("Hello from transpose!")
 }
+
+// Parse out the configuration file
+// Determine which mode we're going to be running
+
+// toNATS
+// Startup the http server to accept incoming cloudevents
+// Verify connectivity with NATS
+
+// fromNATS
+// Startup the NATS client to listen for incoming events
